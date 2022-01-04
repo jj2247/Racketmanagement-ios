b8524b789009e75ef4685c9a7039628dd9217f30
jj2247 <jj2247@student.uni-lj.si>
2022-01-04 12:48:28 +0100

    Initial Commit

---
+++ racketmanagement/racketmanagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  racketmanagementApp.swift
+//  racketmanagement
+//
+//  Created by Jasa Jovan on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct racketmanagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
